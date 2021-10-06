ca286e0f83db9278740c7c03e3d9a59be4cd587d
MaheenAnees <ma05156@st.habib.edu.pk>
2021-10-06 20:15:27 +0500

    Implemented Simulated Annealing

--- Q2/SimulatedAnnealing.py
+++ Q2/SimulatedAnnealing.py
@@ -4,8 +4,8 @@ import random
 from matplotlib import pyplot as plt
 
 class SimulatedAnnealing:
-    def __init__(self, function, limits, minTemp, factor, iterations, stepSize):
-        self.function = function
+    def __init__(self, function, limits, minTemp, factor, iterations, stepSize, opType):
+        self.function = function 
         self.xLower = limits[0]
         self.yLower = limits[1]
         self.xUpper = limits[2]
@@ -15,12 +15,25 @@ class SimulatedAnnealing:
         self.iterations = iterations
         self.xcoordLst = []
         self.ycoordLst = []
-        self.coordLst = []
+        self.funcLst = []
         self.stepSize = stepSize
+        self.opType = opType    #the operation type: either max or min
+
+    def checkBoundary(self, x, y):
+        if  x >= self.xLower and x <= self.xUpper and y >= self.yLower and y <= self.yUpper:
+            return True
 
     def returnNeighbour(self, x, y):
+        #defined neighbourhood with all possible moves
         neighbourhood = [(x + self.stepSize, y), (x - self.stepSize, y), (x, y + self.stepSize), (x, y - self.stepSize),
                  (x - self.stepSize, y - self.stepSize), (x + self.stepSize, y + self.stepSize), (x - self.stepSize, y + self.stepSize), (x + self.stepSize, y - self.stepSize)]
+        validMoves = []
+        #for every neighbour check whether its in the boundary to consider it as valid move
+        for i in range(len(neighbourhood)):
+            if self.checkBoundary(neighbourhood[i][0], neighbourhood[i][1]) == True:
+                validMoves.append(neighbourhood[i])
+        #return a random move from valid moves        
+        return validMoves
     
     def algorithm(self):
         #choose initial position randomly within given limits  
@@ -28,37 +41,32 @@ class SimulatedAnnealing:
         y = random.randint(self.yLower, self.yUpper)
         temp = self.function(x, y) * 0.2
 
-        #add the initial x,y coords in the plot list for visualization
-        self.xcoordLst.append(x)
-        self.ycoordLst.append(y)
-
         while(temp > self.minTemp):
             for i in range(self.iterations):
+                #add the x,y, f coords in the plot list for visualization
+                self.xcoordLst.append(x)
+                self.ycoordLst.append(y)
+                self.funcLst.append(self.function(x,y))
                 #choose a random neighbour
-                newX, newY = self.returnNeighbour()
+                newX, newY = random.choice(self.returnNeighbour(x,y))
                 delta = self.function(newX, newY) - self.function(x, y)
-                if delta > 0:
+                if delta > 0 and self.opType == 'max': #for maximum, f(xNew, yNew) should be more than f(x,y)
                     x, y = newX, newY
+                elif delta < 0 and self.opType == 'min': #for minimum, f(xNew, yNew) should be less than f(x,y)
+                    x, y = newX, newY 
                 else:
-                    m = math.exp((delta/temp))
+                    m = math.exp(-(abs(delta)/temp))
                     p = random.uniform(0,1)
                     if p < m:
                         x, y = newX, newY
             #decrease the temp by given factor
             temp = self.coolingFactor * temp
-            #add the updated/new x,y in plot lists for visualization
-            self.xcoordLst.append(x)
-            self.ycoordLst.append(y)
-            self.coordLst.append(self.function(x,y))
-
-
-
-
+        
 
 
     def visualize(self):
         f, (ax1, ax2) = plt.subplots(2, 1)
-        ax1.plot(self.coordLst, marker=".", color="red", label="objective")
+        ax1.plot(self.funcLst, marker=".", color="red", label="objective")
         ax2.plot(self.xcoordLst, marker=".", label="x1", color="blue")
         ax2.plot(self.ycoordLst, linestyle="dashed", color="green",  label="x2")
         ax1.legend(), ax2.legend()
@@ -76,5 +84,6 @@ def griewankFunc(x,y):
     return (((x ** 2) + (y ** 2)) / 4000) - math.cos(x) * math.cos(y / math.sqrt(2)) + 1
 
 
-
-
+func1 = SimulatedAnnealing(function=sphereFunc, limits=[0,0,100,100], minTemp = 0.0000001, factor=0.9, iterations=100, stepSize=0.1, opType='max')
+func1.algorithm()
+func1.visualize()
